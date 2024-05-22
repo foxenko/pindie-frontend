72dab31d980ffa211e8f10a157991fd0e0f58a95
foxenko <ilyabro65410@gmail.com>
2024-05-23 00:58:22 +0500

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -3,5 +3,5 @@ export const BASE_URL = "https://pindie1backend.nomoredomainswork.ru/api";
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
+  me: `${BASE_URL}/auth/me`,
 };
